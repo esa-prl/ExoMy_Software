64b56cd06172cb7685e63d170c0bd750d6073d84
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-01-24 00:08:08 +0100

    Debugged the Ackermann driving mode

--- src/rover.py
+++ src/rover.py
@@ -4,6 +4,7 @@ import time
 import math
 import enum
 from locomotion_modes import LocomotionMode
+import numpy as np
 
 
 class Rover():
@@ -20,14 +21,15 @@ class Rover():
     def __init__(self):
         self.locomotion_mode = LocomotionMode.FAKE_ACKERMANN
 
-        self.wheel_x = 14.0
-        self.wheel_y = 16.0
+        self.wheel_x = 12.0
+        self.wheel_y = 20.0
 
-        max_steering_angle = 50
+
+        max_steering_angle = 60
         self.ackermann_r_min = abs(
             self.wheel_y) / math.tan(max_steering_angle * math.pi / 180.0) + self.wheel_x
 
-        self.ackermann_r_max = 200
+        self.ackermann_r_max = 250
 
     def setLocomotionMode(self, locomotion_mode_command):
         '''
@@ -107,16 +109,20 @@ class Rover():
 
             return steering_angles
         if(self.locomotion_mode == LocomotionMode.ACKERMANN.value):
-            # Scale between min and max Ackermann radius
-            if steering_command == 0:
-                r = self.ackermann_r_max
-            elif -100 <= steering_command <= 100:
-                r = self.ackermann_r_max - \
-                    abs(steering_command) * \
-                    ((self.ackermann_r_max-self.ackermann_r_min)/100)
-            else:
-                r = 250
+            
+            # No steering if robot is not driving
+            if(driving_command is 0):
+                return steering_angles
 
+
+            # Scale between min and max Ackermann radius
+            if math.cos(math.radians(steering_command)) == 0:
+                r = self.ackermann_r_max
+            else:
+                r = self.ackermann_r_max - \
+                    abs(math.cos(math.radians(steering_command))) * \
+                    ((self.ackermann_r_max-self.ackermann_r_min))
+                
             # No steering
             if r == self.ackermann_r_max:
                 return steering_angles
@@ -126,17 +132,20 @@ class Rover():
             outer_angle = int(math.degrees(
                 math.atan(self.wheel_x/(abs(r)+self.wheel_y))))
 
-            if steering_command > 0:
+            if steering_command > 90 or steering_command < -90:
                 # Steering to the right
+                print('right turn')
                 steering_angles[self.FL] = outer_angle
                 steering_angles[self.FR] = inner_angle
                 steering_angles[self.RL] = -outer_angle
                 steering_angles[self.RR] = -inner_angle
             else:
-                steering_angles[self.FL] = -outer_angle
-                steering_angles[self.FR] = -inner_angle
-                steering_angles[self.RL] = outer_angle
-                steering_angles[self.RR] = inner_angle
+                # Steering to the left
+                print('left turn')
+                steering_angles[self.FL] = -inner_angle
+                steering_angles[self.FR] = -outer_angle
+                steering_angles[self.RL] = inner_angle
+                steering_angles[self.RR] = outer_angle
 
             return steering_angles
 
@@ -156,6 +165,8 @@ class Rover():
                 elif(steering_command <= 0): 
                     wheel_direction = steering_command + 90
                  
+                wheel_direction = np.clip(wheel_direction, -45, 45)
+
                 steering_angles[self.FL] = wheel_direction
                 steering_angles[self.FR] = wheel_direction
                 steering_angles[self.CL] = wheel_direction
@@ -196,38 +207,44 @@ class Rover():
 
         if (self.locomotion_mode == LocomotionMode.ACKERMANN.value):
             v = driving_command
-            radius = self.ackermann_r_max - \
-                abs(steering_command) * \
-                ((self.ackermann_r_max-self.ackermann_r_min)/100)
+            if(steering_command < 0):
+                v *=-1
 
+            # Scale between min and max Ackermann radius
+            radius = self.ackermann_r_max - \
+                abs(math.cos(math.radians(steering_command))) * \
+                ((self.ackermann_r_max-self.ackermann_r_min))
+ 
             if (v == 0):
                 return motor_speeds
 
-            if (abs(steering_command) <= self.ackermann_r_min):
+            if (radius == self.ackermann_r_max):
                 return [v] * 6
             else:
                 rmax = radius + self.wheel_x
-                if radius < 0:
-                    rmax *= -1
 
                 a = math.pow(self.wheel_y, 2)
-                b = math.pow(self.wheel_y, 2)
-                c = math.pow(abs(radius) + self.wheel_x, 2)
-                d = math.pow(abs(radius) - self.wheel_x, 2)
-                e = abs(radius) - self.wheel_x
+                b = math.pow(abs(radius) + self.wheel_x, 2)
+                c = math.pow(abs(radius) - self.wheel_x, 2)
                 rmax_float = float(rmax)
 
-                v1 = int(v*(math.sqrt(b + d))/rmax_float)
-                # Slowest wheel
-                v2 = int((v*e/rmax_float))
-                v3 = int((v*math.sqrt(a + d))/rmax_float)
-                v4 = int((v*math.sqrt(a + c))/rmax_float)
-                v5 = int(v)                            # Fastest wheel
-                v6 = int((v*math.sqrt(b + c))/rmax_float)
+                r1 = math.sqrt(a+b)
+                r2 = rmax_float
+                r3 = r1
+                r4 = math.sqrt(a+c)
+                r5 = abs(radius) - self.wheel_x  
+                r6 = r4
 
-                if (steering_command < 0):
+                v1 = int(v) 
+                v2 = int(v*r2/r1) 
+                v3 = v1
+                v4 = int(v*r4/r1) 
+                v5 = int(v*r5/r1)
+                v6 = v4 
+
+                if (steering_command > 90 or steering_command < -90):
                     motor_speeds = [v1, v2, v3, v4, v5, v6]
-                elif (steering_command > 0):
+                else:
                     motor_speeds = [v6, v5, v4, v3, v2, v1]
 
                 return motor_speeds
