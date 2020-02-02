0fb56d62a50115e3f926da7f91506990a1ccab41
Martin Azkarate <martin.azkarate@gmail.com>
2020-02-02 18:06:45 +0100

    Added robot_node

---
+++ exomy/robot_node.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import time
+from exomy.msg import Joystick, Commands, Screen
+import rclpy
+from rclpy.node import Node
+from rover import Rover
+import message_filters
+
+class RobotNode(Node):
+    def __init__(self):
+        super.__init__('robot_node')
+        
+        self.joy_sub = self.create_subscription(
+            Joystick,
+            '/joystick',
+            self.joy_callback,
+            10)
+
+        self.robot_pub = self.create_publisher(
+            Commands,
+            '/robot_commands',
+            1)
+        self.robot = Rover()
+        
+    def joy_callback(self,msg):
+        cmds = Commands()
+
+        self.robot.setLocomotionMode(msg.locomotion_mode)
+        cmds.motor_angles = self.robot.joystickToSteeringAngle(
+            msg.vel,msg.steering)
+        cmds.motor_speeds = self.robot.joystickToVelocity(msg.vel, msg.steering)
+
+        self.robot_pub.publish(cmds)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    robot_node = RobotNode()
+
+    rclpy.spin(robot_node)        
+
+    rover_node.destroy_node()
+
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
