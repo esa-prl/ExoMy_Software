aa2f0a4f061b3e866175520792487f7d0bdfa74c
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-02-03 14:24:02 +0100

    Added launch file

---
+++ launch/exomy.launch.py
@@ -0,0 +1,39 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+namespace_ = 'exomy'
+
+
+def generate_launch_description():
+    robot = Node(
+        package='exomy',
+        node_executable='robot_node',
+        node_name='robot_node',
+        node_namespace=namespace_,
+        output='screen'
+    )
+    joystick = Node(
+        package='exomy',
+        node_executable='joystick_node',
+        node_name='joystick_node',
+        node_namespace=namespace_,
+        output='screen'
+    )
+    motors = Node(
+        package='exomy',
+        node_executable='motor_node',
+        node_name='motor_node',
+        node_namespace=namespace_,
+        output='screen'
+    )
+
+    return LaunchDescription([
+        robot,
+        joystick,
+        motors
+    ])
