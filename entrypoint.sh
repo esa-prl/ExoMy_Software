54cceacc1a3f61ef594056acf4d460f1258e037d
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-02-27 11:15:50 +0100

    Added http-server to entrypoint.sh script to run it in the background on launch.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 source /opt/ros/melodic/setup.bash
 cd /root/exomy_ws
 catkin_make
-source devel/setup.bash
+http-server src/exomy/gui -p 8000 &
 
+source devel/setup.bash
 roslaunch exomy exomy.launch
+
+exec "$@"
