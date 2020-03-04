8c2830595ac764f51cbae21aaf2d451f362bacfb
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-03-04 15:53:23 +0100

    Added config parameter to entrypoint shell script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-source /opt/ros/melodic/setup.bash
-cd /root/exomy_ws
-catkin_make
-http-server src/exomy/gui -p 8000 &
+if [ $1 == "config" ]
+then
+	bash
+else
+	source /opt/ros/melodic/setup.bash
+	cd /root/exomy_ws
+	catkin_make
+	http-server src/exomy/gui -p 8000 &
 
-source devel/setup.bash
-roslaunch exomy exomy.launch
+	source devel/setup.bash
+	roslaunch exomy exomy.launch
 
-exec "$@"
+	bash
+fi
+
