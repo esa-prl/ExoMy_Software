8bee07a68ef2f03a85103d5175f45f2d47deaa1e
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-03-05 14:26:33 +0100

    Added more parameters to docker run

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 if [ $1 == "config" ]
 then
 	bash
-else
+elif [ $1 == "autostart" ]
 	source /opt/ros/melodic/setup.bash
 	cd /root/exomy_ws
 	catkin_make
@@ -11,6 +11,13 @@ else
 	source devel/setup.bash
 	roslaunch exomy exomy.launch
 
+	bash
+elif [ $1 == "ros" ]
+	cd /root/exomy_ws
+	catkin_make
+	source devel/setup.bash
+	bash
+else
 	bash
 fi
 
