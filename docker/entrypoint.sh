c1e8cae43a371f2b3e33103c20dbab4a05480361
Miro Voellmy <miro.voellmy@esa.int>
2020-06-04 17:44:35 +0200

    fixed entrypoint file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [ $1 == "config" ]
 then
+	cd /root/exomy_ws/src/exomy/scripts
 	bash
 elif [ $1 == "autostart" ]
+then
 	source /opt/ros/melodic/setup.bash
 	cd /root/exomy_ws
 	catkin_make
@@ -13,7 +15,9 @@ elif [ $1 == "autostart" ]
 
 	bash
 elif [ $1 == "ros" ]
+then
 	cd /root/exomy_ws
+	source /opt/ros/melodic/setup.bash
 	catkin_make
 	source devel/setup.bash
 	bash
