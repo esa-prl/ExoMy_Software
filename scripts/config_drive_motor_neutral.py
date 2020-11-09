2c99ebbb64ab9b026b67abc04e11524ad987a6cb
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-11-09 16:09:43 +0100

    Add the read out from config file to config_drive_motor_pwm_neutral script

--- scripts/config_drive_motor_neutral.py
+++ scripts/config_drive_motor_neutral.py
@@ -15,6 +15,19 @@ def get_driving_pins():
             pin_list.append(value)
     return pin_list
 
+def get_drive_pwm_neutral():
+     
+    with open(config_filename, 'r') as file:
+        param_dict = yaml.load(file)
+
+    for key, value in param_dict.items():
+        if('drive_pwm_neutral' in str(key)):
+            return value                    
+
+    default_value = 300
+    print('The parameter drive_pwm_neutral could not be found in the exomy.yaml \n')
+    print('It was set to the default value: '+ default_value + '\n')
+    return default_value
 
 if __name__ == "__main__":
     print(
@@ -34,13 +47,19 @@ $$$$$$$$\ $$  /\$$\ \$$$$$$  |$$ | \_/ $$ |\$$$$$$$ |
     )
     print(
         '''
-This script helps you to set the offset of the driving motors correctly.
+This script helps you to set the neutral values of PWM of the driving motors correctly.
 It will send the intended signal for "not moving" to all the motors.
 On each motor you have to turn the correction screw until the motor really stands still.
         '''
     )
 
     pwm = Adafruit_PCA9685.PCA9685()
+
+    '''
+    The drive_pwm_neutral value is determined from the exomy.yaml file.
+    But it can be also calculated from the values of the PWM board and motors, 
+    like shown in the following calculation:
+
     # For most motors a pwm frequency of 50Hz is normal
     pwm_frequency = 50.0  # Hz
     pwm.set_pwm_freq(pwm_frequency)
@@ -56,7 +75,9 @@ On each motor you have to turn the correction screw until the motor really stand
 
     # The PCA 9685 board requests a 12 bit number for the duty_cycle
     value = int(duty_cycle*4096.0) # 307
+    '''
 
+    value = get_drive_pwm_neutral()
     pin_list = get_driving_pins()
 
     for pin in pin_list:
